ab859fd2bebcbd9eaa2d0b79a412cc3469323b98
Stuart Penrose <stu@penrose.us>
2013-02-21 15:02:58 -0800

    Initial

---
+++ src/main/resources/sample-data.js
@@ -0,0 +1,30 @@
+
+ {
+	 "id":"4702bb26-ca43-405c-b74e-1a877f2005ad",
+	 "name":"skyscraper 1.0",
+	 "items":[
+	     {
+	    	 "id":"98cf0c34-9e03-43db-a281-2102dc4f93ab",
+	    	 "name":"build foundation",
+	    	 "kind":"epic",
+	    	 "estimates":[
+               {
+            	   "id":"98cf0c34-9e03-43db-a281-2102dc4f93ab",
+            	   "currency":"team", 
+            	   "value":34, 
+            	   "when":234324333
+               }
+	    	 ],
+	    	 "items":[
+	    	     {
+	    	       "name":"put-up forms",
+	    	       "estimates":[
+	    	            {"currency":"team", "value":34, "when":234324333}
+	    	        ]
+	    	     }
+	    	 ]
+	     },
+	     {"id":"", "kind":"goal", "name":"release it!"}
+	 ]
+ }
+ 
---
+++ src/main/scala/com/cj/etherlog/Etherlog.scala
@@ -0,0 +1,24 @@
+
+package com.cj.etherlog
+
+import org.httpobjects.jetty.HttpObjectsJettyHandler
+import org.httpobjects.HttpObject
+import org.httpobjects.DSL._
+import org.httpobjects.Request
+import org.httpobjects.util.ClasspathResourcesObject
+import org.httpobjects.util.ClasspathResourceObject
+
+object Etherlog {
+  def main(args: Array[String]) {
+    HttpObjectsJettyHandler.launchServer(8080, 
+        new HttpObject("/api/backlogs/{id}"){
+            override def get(req:Request) = {
+              OK(Bytes("application/json", getClass().getResourceAsStream("/sample-data.js")))
+            }
+        },
+        new ClasspathResourceObject("/mockup", "/content/backlog-mockup.html", getClass()),
+        new ClasspathResourceObject("/", "/content/backlog.html", getClass()),
+        new ClasspathResourcesObject("/{resource}", getClass(), "/content")
+    ); 
+  }
+}
