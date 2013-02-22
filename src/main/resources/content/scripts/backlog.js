a4d08c2a1d1729116275788a69249e0c850d4ce9
Stuart Penrose <stu@penrose.us>
2013-02-21 23:28:48 -0800

    Got basic crud & drag-n-drop ordering working.

--- src/main/resources/content/scripts/backlog.js
+++ src/main/resources/content/scripts/backlog.js
@@ -1,7 +1,7 @@
-define(["jquery", "http"], function($, http){
+define(["jquery", "http", "uuid"], function($, http, uuid){
 	  
 	
-	  var backlog, where;
+	  var backlog, where, lastDragged;
 	  
 	  var backlogId = 23; // HACK!
 	  
@@ -15,16 +15,17 @@ define(["jquery", "http"], function($, http){
 	      slider : where.find("#slider"),
 	      editButton : where.find(".edit-button"),
 	      saveButton : where.find(".save-button"),
+	      addButton : where.find(".add-button"),
 	      commitMessage : where.find(".commit-message")
 	  };
 	  
 	  
 	  
 	  function render(){
-		  console.log(backlog);
 		  view.title.text(backlog.name);
 		  view.backlog.empty();
 		  $.each(backlog.items, function(idx, item){
+			  DropZone(item.id, view.backlog);
 			  widgets.push(ItemWidget(item, view.backlog));
 		  });
 	  }
@@ -32,33 +33,91 @@ define(["jquery", "http"], function($, http){
 	  function showEditMode(){
 		  
 		  $.each(widgets, function(idx, widget){
-			  widget.showEditMode();
+			  widget.showEditableMode();
 		  });
 		  
-	      where.find( ".project-chunk" ).draggable();
-	      where.find(".milestone").draggable();
 	      view.editButton.hide();
 	      view.commitMessage.show();
 	      view.saveButton.show();
+	      view.addButton.show();
 	  }
 	  
 	  function showViewMode(){
 	      view.editButton.show();
 	      view.commitMessage.hide();
 	      view.saveButton.hide();
+	      view.addButton.hide();
 	      render();
 	  }
 	  
+	  function deleteItem(item){
+		  var idx = findItemNumById(item.id);
+		  console.log("Deleting #" + idx + " " + JSON.stringify(item));
+		  
+		  backlog.items.splice(idx, 1);
+		  where.find("#" + item.id).remove();
+	  }
+	  
+	  function moveItemBefore(itemId, beforeId){
+		  if(itemId!==beforeId){
+	        	var subject = where.find("#" + itemId); 
+	        	
+	        	subject.detach().insertBefore("#dropZone" + beforeId);
+	        	subject.css("left", 0);
+	        	subject.css("top", 0);
+	        	$("#dropZone" + itemId).detach().insertBefore(where.find("#" + itemId));
+	        }
+	  }
+	  
+	  function DropZone(id, backlogDiv){
+		  var v = $('<div id="dropZone' + id + '" class="drop-zone"></div>');
+		  
+
+		  function show(){
+			  v.css("visibility", "visible");
+		  }
+		  function hide(){
+			  v.css("visibility", "hidden");
+		  }
+		  
+		  function currentDropIsAcceptable(){
+			  return lastDragged.id !==id;
+		  }
+		  
+		  v.droppable({
+		      drop: function( event, ui ) {
+		        console.log(lastDragged.name + " was dropped on " + id);
+		        var subjectId = lastDragged.id;
+		        moveItemBefore(subjectId, id);
+		        hide();
+		      },
+		      over: function(event, ui){
+		    	  if(currentDropIsAcceptable()){
+		    		  show();
+		    	  }
+		      },
+		      out: function(event, ui){
+		    	  hide();
+		      }
+		  });
+		  
+		  hide();
+		  v.appendTo(backlogDiv);
+	  }
 	  
 	  function ItemWidget(item, backlogDiv){
-		  var html, v, view;
-
-		  v = $('<div id="' + item.id + '" class="item"><span class="label"/><textarea style="display:none;"></textarea><img style="display:none;" src="/pencil.png"/ class="edit-icon edit-button"><button style="display:none;" class="done-button">Done</button></div>');
+		  var html, v, view, onDelete;
+		  
+		  v = $('<div id="' + item.id + '" class="item clearfix">' + 
+				  '<img style="display:none;" src="/delete.png"/ class="delete-icon delete-button">' + 
+				  '<img style="display:none;" src="/pencil.png"/ class="edit-icon edit-button">' + 
+				  '<button style="display:none;" class="done-button">Done</button><span class="label"/><textarea style="display:none;"></textarea></div>');
 		  
 		  view = {
 		      label:v.find(".label"),
 		      textarea:v.find("textarea"),
 		      editButton:v.find(".edit-button"),
+		      deleteButton:v.find(".delete-button"),
 		      doneButton:v.find(".done-button")
 		  };
 		  
@@ -81,11 +140,28 @@ define(["jquery", "http"], function($, http){
 			  console.log("Changed to " + item.name);
 		  });
 		  
+		  function makeDraggable(){
+
+			  v.draggable({
+				  start: function( event, ui ) {
+					  console.log("Started dragging " + item.name);
+					  lastDragged = item;
+				  	},
+				   stop:function(event, ui){
+					   console.log("Stopped dragging" + ui.helper.attr('id'));
+					   ui.helper.css("left", 0);
+					   ui.helper.css("top", 0);
+				   }
+			  });
+		  }
+		  
 		  function showEditableMode(){
 			  view.editButton.show();
 			  view.label.show();
 			  view.textarea.hide();
 			  view.doneButton.hide();
+			  view.deleteButton.show();
+			  makeDraggable();
 		  }
 
 		  function showEditMode(){
@@ -93,17 +169,57 @@ define(["jquery", "http"], function($, http){
 			  view.textarea.show();
 			  view.editButton.hide();
 			  view.doneButton.show();
+			  view.deleteButton.hide();
+			  makeDraggable();
 		  }
 		  
 		  view.doneButton.button().click(showEditableMode);
 		  
 		  view.editButton.click(showEditMode);
+		  view.deleteButton.click(function(){
+			  deleteItem(item);
+		  });
 		  
 		  v.appendTo(backlogDiv);
 		  
+		  
+		  function scrollTo(){
+			  $('html, body').animate({
+		            scrollTop: v.offset().top + 'px'
+		        }, 'fast');
+		  }
+		  
+		  
 		  return {
-			  showEditMode:showEditableMode
+			  showEditMode:showEditMode,
+			  showEditableMode:showEditableMode,
+			  scrollTo:scrollTo
 		  };
+	  }
+	  
+	  function findItemNumById(id){
+		  var result;
+		  
+		  $.each(backlog.items, function(idx, item){
+			  if(item.id===id){
+				  result = idx;
+			  }
+			  
+		  });
+		  
+		  return result;
+	  }
+	  
+	  function findItemById(id){
+		  console.log("Looking for" + id);
+		  var matches = $.grep(backlog.items, function(item){
+			  console.log(item);
+			  const result = item.id===id;
+			  console.log(result);
+			  return result;
+		  });
+		  
+		  return matches[0];
 	  }
 	  
 	  function readView(){
@@ -112,16 +228,8 @@ define(["jquery", "http"], function($, http){
 		  
 		  where.find(".project-chunk, .milestone").each(function(idx, domElement){
 			  var id = $(domElement).attr("id");
-			  console.log("Looking for" + id);
-			  var matches = $.grep(backlog.items, function(item){
-				  console.log(item);
-				  const result = item.id===id;
-				  console.log(result);
-				  return result;
-			  });
+			  var chunk = findItemById(id);
 			  
-			  var chunk = matches[0];
-
 			  console.log("Result: " + JSON.stringify(chunk));
 			  console.log("chunk: " + chunk.id + " " + chunk.name + " (" + chunk.kind + ")");
 			  
@@ -153,6 +261,23 @@ define(["jquery", "http"], function($, http){
 	  });
 	  
 
+	  view.addButton.button().click(function(){
+		  const item = {
+				  id:uuid(),
+				  name:"new story",
+				  kind:"story"
+		  };
+		  console.log(item);
+		  DropZone(item.id, view.backlog);
+		  const widget = ItemWidget(item, view.backlog);
+		  widget.showEditMode();
+		  widgets.push(widget);
+		  backlog.items.push(item);
+		  
+		  
+		  widget.scrollTo();
+	  });
+	  
 	  http({
 		  url: "/api/backlogs/" + backlogId,
           method: "GET",
